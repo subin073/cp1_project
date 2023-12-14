e96efdb787b931173770cc294e8124fc0c7e8b88
sumin <sumint730@naver.com>
2023-12-14 14:42:56 +0900

    초기 커밋.

---
+++ cp1_project/func.c
@@ -0,0 +1,30 @@
+////func.c 파일
+//#include "main.h"
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <windows.h>
+//
+//int initializeSensor(SensorData* data) {
+//    // 가상 센서 초기화 코드
+//
+//    // 예시로 초기화 성공을 출력
+//    printf("Virtual Sensor initialized successfully.\n");
+//
+//    // 예시로 랜덤한 값을 생성
+//    data->temperature = (float)(rand() % 50);
+//    data->humidity = (float)(rand() % 100);
+//
+//    // 성공 시 0, 실패 시 -1 반환
+//    return 0;
+//}
+//
+//int readSensor(SensorData* data) {
+//    // 가상 센서에서 데이터 읽기 코드
+//
+//    // 예시로 랜덤한 값을 생성
+//    data->temperature = (float)(rand() % 50);
+//    data->humidity = (float)(rand() % 100);
+//
+//    // 성공 시 0, 실패 시 -1 반환
+//    return 0;
+//}
